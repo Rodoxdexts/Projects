ad24834e03a0ce5cd600b28918a94b3669367d67
Rodoxdexts <email.m.l.costa@gmail.com>
2024-01-21 21:39:33 -0300

    a

---
+++ Informações de uma pessoa.py
@@ -0,0 +1,3 @@
+nome1 = input('Olá usuário, que bom te conhecer\nQual o seu nome, ')
+print(f'Que prazer conhecê-lo {nome1}!')
+nome2 = input('Sou uma máquina, pode me dar um nome?')
---
+++ Par ou Ímpar.py
@@ -0,0 +1,4 @@
+# import math
+print('Seja bem vindo ao Par ou Ímpar')
+número = int(input('Digite um número inteiro que seja par ou ímpar; '))
+
